49b8d4d83e4f12b80d0d2494f1a0fc5ee6b7abaa
Jaspreet Singh <sfs.jaspreetsingh24@gmail.com>
2025-07-23 12:19:37 +0530

    fixed issues of schema

--- src/users/entities/user-restrictions.entity.ts
+++ src/users/entities/user-restrictions.entity.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { Types } from "mongoose";
+import { Document, Types } from "mongoose";
 import { User } from "./user.entity";
 import { UserRestrictionType } from "src/helpers/enums";
 
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,5 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
 import { LoginTypeEnum, userRole } from "src/helpers/enums";
 
 @Schema({ timestamps: true })
